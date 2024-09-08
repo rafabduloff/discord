43999f9a9e4d09da4928d4e1220edd71ee131460
rafabduloff <136557397+rafabduloff@users.noreply.github.com>
2024-09-08 15:59:54 +0500

    Add files via upload

---
+++ json_visualizer.py
@@ -0,0 +1,18 @@
+﻿import time
+import requests
+
+url = "https://t2-gifts.ru/"
+
+def check_website(url):
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            print(f"Сайт {url} работает.")
+        else:
+            print(f"Сайт {url} недоступен. Статус код: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка подключения к сайту {url}: {e}")
+
+while True:
+    check_website(url)
+    time.sleep(5)
